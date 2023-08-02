5fa5246d3709382b014a5b46b467b08cae26d849
Carlos Molina <molinacarlosj@gmail.com>
2023-08-01 22:29:40 -0400

    first commit

---
+++ src/components/InvoiceApp.jsx
@@ -0,0 +1,40 @@
+import { getInvoice } from "../services/getInvoice"
+import { ClientView } from "./ClientView";
+import { CompanyView } from "./CompanyView";
+import { InvoiceView } from "./InvoiceView";
+import { ListItemsView } from "./ListItemsView";
+
+export const InvoiceApp = () => {
+
+    const { id, name, client, company, items } = getInvoice();
+
+    return (
+        <>
+            <div className="container">
+
+                <div className="card my-3">
+
+                    <div className="card-header">
+                        Ejemplo Factura
+                    </div>
+                    <div className="card-body">
+                        <InvoiceView id={ id } name={ name }/>
+
+                        <div className="row my-3">
+
+                            <div className="col">
+                               <ClientView title="Datos del cliente" client ={ client }/>
+                            </div>
+
+                            <div className="col">
+                                <CompanyView title="Datos de la Empresa" company={ company }/>
+                            </div>
+
+                        </div>
+                        <ListItemsView title="Productos de la factura" items={items}/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/ListItemsView.jsx
@@ -0,0 +1,28 @@
+import { RowItemView } from "./RowItemView";
+
+export const ListItemsView = ({ title, items }) => {
+  return (
+    <>
+      <h4>{title}</h4>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(({ id, product, price, quantity }) => (
+            <RowItemView
+              key={id}
+              product={product}
+              price={price}
+              quantity={quantity}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
