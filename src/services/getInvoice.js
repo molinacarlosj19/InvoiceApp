d9f236bc73b15f15d5cb585ec76e2d8b66402451
Carlos Molina <molinacarlosj@gmail.com>
2023-08-09 22:29:41 -0400

    Calculate total with map reduce

--- src/services/getInvoice.js
+++ src/services/getInvoice.js
@@ -5,6 +5,7 @@ export const getInvoice = () =>{
     // invoice.items.forEach(item => {
     //     total = total + item.price * item.quantity;
     // });
+    
     const total = invoice.items
         .map( item => item.price * item.quantity)
         .reduce( (accumulator, currentValue) => accumulator + currentValue, 0 );
