9bd9cb0483701ff61956f9d0d5c9581b7992044e
Carlos Molina <molinacarlosj@gmail.com>
2023-08-11 20:15:05 -0400

    Managing form state with useState

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getInvoice } from "./services/getInvoice";
 import { ClientView } from "./components/ClientView";
 import { CompanyView } from "./components/CompanyView";
@@ -6,37 +7,68 @@ import { ListItemsView } from "./components/ListItemsView";
 import { TotalView } from "./components/TotalView";
 
 export const InvoiceApp = () => {
+  const { id, total, name, client, company, items: itemsInitial } = getInvoice();
+  const [priceValue, setPriceValue] = useState(0);
+  const [productValue, setProducValue] = useState('');
+  const [quantityValue, setQuantityValue] = useState(0);
+  const [items, setItems] = useState(itemsInitial);
 
-    const { id, total, name, client, company, items } = getInvoice();
+  return (
+    <>
+      <div className="container">
+        <div className="card my-3">
+          <div className="card-header">Ejemplo Factura</div>
+          <div className="card-body">
+            <InvoiceView id={id} name={name} />
 
-    return (
-        <>
-            <div className="container">
+            <div className="row my-3">
+              <div className="col">
+                <ClientView title="Datos del cliente" client={client} />
+              </div>
 
-                <div className="card my-3">
-
-                    <div className="card-header">
-                        Ejemplo Factura
-                    </div>
-                    <div className="card-body">
-                        <InvoiceView id={ id } name={ name }/>
-
-                        <div className="row my-3">
-
-                            <div className="col">
-                               <ClientView title="Datos del cliente" client ={ client }/>
-                            </div>
-
-                            <div className="col">
-                                <CompanyView title="Datos de la Empresa" company={ company }/>
-                            </div>
-
-                        </div>
-                        <ListItemsView title="Productos de la factura" items={items}/>
-                        <TotalView total= {total}/>
-                    </div>
-                </div>
+              <div className="col">
+                <CompanyView title="Datos de la Empresa" company={company} />
+              </div>
             </div>
-        </>
-    )
-}
+            <ListItemsView title="Productos de la factura" items={items} />
+            <TotalView total={total} />
+            <form className="w-50" onSubmit={ event =>{
+                event.preventDefault();
+
+                setItems([...items, {key: 4, product: productValue, price: priceValue, quantity: quantityValue}]);
+            }}>
+              <input
+                type="text"
+                name="product"
+                placeholder="Producto"
+                className="form-control m-3" onChange={ event => {
+                    console.log(event.target.value);
+                    setProducValue(event.target.value);
+                }}
+              />
+              <input
+                type="text"
+                name="price"
+                placeholder="Precio"
+                className="form-control m-3" onChange={ event => {
+                    console.log(event.target.value);
+                    setPriceValue(event.target.value);
+                }}
+              />
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Cantidad"
+                className="form-control m-3" onChange={ event => {
+                    console.log(event.target.value);
+                    setQuantityValue(event.target.value);
+                }}
+              />
+              <button typeof="submit" className="btn btn-primary">Crear Item</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
