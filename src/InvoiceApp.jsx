a0233daeb2a92fcfe6e87f2998a7ef2ef3ecce15
Carlos Molina <molinacarlosj@gmail.com>
2023-08-13 22:39:35 -0400

    Recalculate total, separate form, button hidden form, delete items

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -1,74 +1,114 @@
-import { useState } from "react";
-import { getInvoice } from "./services/getInvoice";
+import { useEffect, useState } from "react";
+import { getInvoice, calculateTotal } from "./services/getInvoice"
 import { ClientView } from "./components/ClientView";
 import { CompanyView } from "./components/CompanyView";
 import { InvoiceView } from "./components/InvoiceView";
 import { ListItemsView } from "./components/ListItemsView";
 import { TotalView } from "./components/TotalView";
+import { FormItemsView } from "./components/FormItemsView";
+
+const invoiceInitial = {
+    id: 0,
+    name: '',
+    client: {
+        name: '',
+        lastName: '',
+        address: {
+            country: '',
+            city: '',
+            street: '',
+            number: 0
+        }
+    },
+    company: {
+        name: '',
+        fiscalNumber: 0,
+    },
+    items: []
+};
 
 export const InvoiceApp = () => {
-  const { id, total, name, client, company, items: itemsInitial } = getInvoice();
-  const [priceValue, setPriceValue] = useState(0);
-  const [productValue, setProducValue] = useState('');
-  const [quantityValue, setQuantityValue] = useState(0);
-  const [items, setItems] = useState(itemsInitial);
 
-  return (
-    <>
-      <div className="container">
-        <div className="card my-3">
-          <div className="card-header">Ejemplo Factura</div>
-          <div className="card-body">
-            <InvoiceView id={id} name={name} />
+    const [activeForm, setActiveForm] = useState(false);
 
-            <div className="row my-3">
-              <div className="col">
-                <ClientView title="Datos del cliente" client={client} />
-              </div>
+    const [total, setTotal] = useState(0);
 
-              <div className="col">
-                <CompanyView title="Datos de la Empresa" company={company} />
-              </div>
+    const [counter, setCounter] = useState(4);
+
+    const [invoice, setInvoice] = useState(invoiceInitial);
+
+    const [items, setItems] = useState([]);
+
+    const { id, name, client, company } = invoice;
+
+    useEffect(() => {
+        const data = getInvoice();
+        console.log(data);
+        setInvoice(data);
+        setItems(data.items);
+    }, []);
+
+    useEffect(() => {
+        // console.log('el counter cambio!')
+    }, [counter]);
+
+    useEffect(() => {
+        setTotal(calculateTotal(items));
+        // console.log('el items cambio!')
+    }, [items]);
+
+    const handlerAddItems = ({ product, price, quantity}) => {
+
+        setItems([...items, {
+            id: counter,
+            product: product.trim(),
+            price: +price.trim(),
+            quantity: parseInt(quantity.trim(), 10)
+        }]);
+
+        setCounter(counter + 1);
+    }
+
+    const handlerDeleteItem = (id) => {
+        setItems(items.filter(item => item.id !== id ))
+    }
+
+    const onActiveForm = () => {
+        setActiveForm(!activeForm);
+    }
+
+    return (
+        <>
+            <div className="container">
+
+                <div className="card my-3">
+
+                    <div className="card-header">
+                        Ejemplo Factura
+                    </div>
+                    <div className="card-body">
+                        <InvoiceView id={ id } name={ name } />
+
+                        <div className="row my-3">
+
+                            <div className="col">
+                                <ClientView title="Datos del cliente" client={client} />
+                            </div>
+
+                            <div className="col">
+                                <CompanyView title="Datos de la empresa" company={company} />
+                            </div>
+
+                        </div>
+
+                        <ListItemsView title="Productos de la factura" items={items} handlerDeleteItem={ id => handlerDeleteItem(id) } />
+                        <TotalView total={total} />
+                        <button className="btn btn-secondary"
+                            onClick={onActiveForm}>{!activeForm ? 'Agregar Item': 'Cerrar Form'}</button>
+                        { !activeForm || <FormItemsView handler={handlerAddItems} /> }
+                    </div>
+                </div>
             </div>
-            <ListItemsView title="Productos de la factura" items={items} />
-            <TotalView total={total} />
-            <form className="w-50" onSubmit={ event =>{
-                event.preventDefault();
-
-                setItems([...items, {key: 4, product: productValue, price: priceValue, quantity: quantityValue}]);
-            }}>
-              <input
-                type="text"
-                name="product"
-                placeholder="Producto"
-                className="form-control m-3" onChange={ event => {
-                    console.log(event.target.value);
-                    setProducValue(event.target.value);
-                }}
-              />
-              <input
-                type="text"
-                name="price"
-                placeholder="Precio"
-                className="form-control m-3" onChange={ event => {
-                    console.log(event.target.value);
-                    setPriceValue(event.target.value);
-                }}
-              />
-              <input
-                type="text"
-                name="quantity"
-                placeholder="Cantidad"
-                className="form-control m-3" onChange={ event => {
-                    console.log(event.target.value);
-                    setQuantityValue(event.target.value);
-                }}
-              />
-              <button typeof="submit" className="btn btn-primary">Crear Item</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+        </>
+    )
+}
--- src/services/getInvoice.js
+++ src/services/getInvoice.js
@@ -1,13 +1,21 @@
-import { invoice } from "../data/invoice";
+import { invoice } from "../data/invoice"
 
-export const getInvoice = () =>{
+export const getInvoice = () => {
+
+    // console.log(invoice);
+    
     // let total = 0;
     // invoice.items.forEach(item => {
     //     total = total + item.price * item.quantity;
     // });
+
+    const total = calculateTotal(invoice.items)
     
-    const total = invoice.items
-        .map( item => item.price * item.quantity)
-        .reduce( (accumulator, currentValue) => accumulator + currentValue, 0 );
-    return {... invoice, total};
+    return {...invoice, total};
+}
+
+export const calculateTotal = (items = []) => {
+    return items
+        .map(item => item.price * item.quantity)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
